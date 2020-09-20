5795cb8fe1245a3a8a6890559de884df854005a1
Sethuraman Srinivasan <sethu10121994@gmail.com>
2020-09-20 21:04:47 +0530

    :recycle: functionality and ui improvements

---
+++ src/components/@core/navigation-menu/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function NavigationMenu() {
+  return (
+    <nav className="container mx-auto flex items-center justify-between flex-wrap bg-white-500 py-4">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <svg
+          className="fill-current h-8 w-8 mr-2 text-teal-500"
+          width="54"
+          height="54"
+          viewBox="0 0 54 54"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
+        </svg>
+        <span className="font-semibold text-xl tracking-tight text-black">
+          todoapp
+        </span>
+      </div>
+    </nav>
+  );
+}
+
+export default NavigationMenu;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,8 @@
-import { apiAddTodo, apiListTodos } from '@services/todo.service';
-import React, { useEffect } from 'react';
-import randomWords from 'random-words';
+import HomePageComponent from '@components/pages/home';
+import React from 'react';
 
 function IndexPage() {
-  useEffect(() => {
-    console.log('[index.tsx] Console ' + randomWords());
-
-    apiListTodos().then((data) => console.log(data));
-  }, []);
-
-  const addTodo = async () => {
-    const randomString = randomWords();
-
-    const data = await apiAddTodo({
-      name: randomString,
-      description: randomString,
-    });
-
-    console.log(data);
-  };
-
-  return (
-    <>
-      <h2 className="text-4xl">Tailwind.css added</h2>
-      <button onClick={addTodo}>Add</button>
-    </>
-  );
+  return <HomePageComponent />;
 }
 
 export default IndexPage;
